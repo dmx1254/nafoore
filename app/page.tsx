db7e679f33ed8fd9459447ffcb0e58c7e961d318
dmx1254 <tipox1254@gmail.com>
2025-01-08 20:04:34 +0000

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="relative mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 carte-membre">
+        <div className="relative mx-auto max-w-2xl lg:py-56 carte-membre">
           <div className="sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-2 sm:px-3 py-1 text-sm leading-6 text-gray-700 ring-1 ring-gray-900/10 hover:ring-gray-900/20 text-center max-md:mt-32">
+            <div className="relative rounded-full px-2 sm:px-3 py-1 text-sm leading-6 text-gray-700 ring-1 ring-gray-900/10 hover:ring-gray-900/20 text-center max-sm:mt-16">
               Rejoindre le mouvement.{" "}
               <Link
                 href="/devenir-membre"
